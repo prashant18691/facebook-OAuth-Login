484f1fb6e513f131fe0926d93223ab53cefee5eb
pupadhya <orchid15.pb1>
2018-08-17 03:08:34 +0530

    facebook Oauth

--- src/main/java/com/devglan/security/FacebookConnectionSignup.java
+++ src/main/java/com/devglan/security/FacebookConnectionSignup.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +19,11 @@ public class FacebookConnectionSignup implements ConnectionSignUp {
     public String execute(Connection<?> connection) {
         User user = new User();
         user.setUsername(connection.getDisplayName());
-        user.setPassword(BCrypt.hashpw(connection.fetchUserProfile().getUsername(), BCrypt.gensalt()));
+        Facebook facebook = (Facebook) connection.getApi();
+        String [] fields = { "id", "email",  "first_name", "last_name" };
+        org.springframework.social.facebook.api.User userProfile = facebook.fetchObject("me", org.springframework.social
+                .facebook.api.User.class, fields);
+        user.setPassword(BCrypt.hashpw(userProfile.getFirstName()+userProfile.getLastName(), BCrypt.gensalt()));
         userService.save(user);
         return user.getUsername();
     }
